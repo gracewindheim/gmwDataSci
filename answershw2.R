21cd90c72566250adba2f6ac73463c3e322f2e74
gracewindheim <gwindhe1@jhu.edu>
2020-02-14 18:56:14 +0000

    working on q4 and 5

--- answershw2.R
+++ answershw2.R
@@ -25,9 +25,26 @@ library(ggplot2)
 classdat = read.csv("/cloud/project/testRepo/classInterests.txt", sep="\t", header=TRUE, stringsAsFactor=FALSE)
 y = data.frame(table(classdat$Year))
 colnames(y) = c("Year", "Count")
-y = data.frame(y)
+yplot = ggplot(data=y, aes(x=Year, y=Count)) + geom_bar(stat="identity")
 
 ## QUESTION 2 PROGRAM PLOT
+p = data.frame(table(classdat$Program))
+colnames(p) = c("Program", "Count")
+pplot = ggplot(data=p, aes(x=Program, y=Count)) + geom_bar(stat="identity")
+
+## QUESTION 3
+mos = data.frame(classdat)
+x = table(mos$Year, mos$Program)
+mosaicplot(x, main = "Class Data by Year and Program", xlab = "Year", ylab = "Program")
+
+## QUESTION 4
+genedat = read.csv("/cloud/project/testRepo/GSE5859_exprs.csv", sep=",")
+g = data.frame(genedat)
+# how to know which column/row is the mean?
+
+## QUESTION 5
+healthdat = read.csv("/cloud/project/testRepo/healthcare-spending.csv", sep=",")
+h = data.frame(healthdat)
+ggplot(data=h, mapping=aes(x="state", y="spending"))
 
 
-yearp = ggplot(data=y, aes(x=Year, y=Count)) + geom_bar(stat="identity")
